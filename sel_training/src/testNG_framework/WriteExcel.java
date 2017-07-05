0d1fe2b74a9e6194e9459fa7de999eaf43f152e3
Shammi <kuldipkumar169@gmail.com>
2017-07-05 17:17:48 +0530

    practise

--- sel_training/src/testNG_framework/WriteExcel.java
+++ sel_training/src/testNG_framework/WriteExcel.java
@@ -19,22 +19,22 @@ public class WriteExcel {
 		XSSFWorkbook wb = new XSSFWorkbook(fis);
 		XSSFSheet sheet1= wb.getSheetAt(0);
 
-		for (int i = 0; i <= 25; i++) {
-			while (i <= 5 && i <= 10) {
+		for (int i = 0; i <= 25; i++)
+		{
+			while (i <= 5 && i <= 10) 
+			{
 				System.out.println("fail");
 			}
-			for (int j = 0; j < 1; j++) {
-
-				sheet1.createRow(i).createCell(0).setCellValue("Pass");
+			for (int j = 0; j < 1; j++) 
+			{
+             	sheet1.createRow(i).createCell(0).setCellValue("Pass");
 				sheet1.getRow(i).createCell(1).setCellValue("fail");
 				FileOutputStream fos = new FileOutputStream(src);
 				wb.write(fos);
 				System.out.print("      ");
-
 			}
 			System.out.println("");
 		}
-
 		wb.close();
 	}
 
