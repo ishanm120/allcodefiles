6c2825f287838b4a721779fec448050d202c4df4
Ishan <Ishan>
2017-07-13 16:37:54 +0530

    new code for fail test run

---
+++ sel_training/testNG_framework/FailRunOne.java
@@ -0,0 +1,13 @@
+package testNG_framework;
+
+import org.testng.annotations.Test;
+
+public class FailRunOne {
+	
+	@Test
+	public void fbpage(){
+		
+		System.out.println("Facbook opened");
+	}
+
+}
---
+++ sel_training/testNG_framework/FailRunTwo.java
@@ -0,0 +1,13 @@
+package testNG_framework;
+
+import org.testng.annotations.Test;
+
+public class FailRunTwo {
+	
+	@Test
+	public void fbpage(){
+		
+		System.out.println("Facbook opened");
+	}
+
+}
