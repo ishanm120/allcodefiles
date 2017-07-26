8a6cf434db6c8b550a69898a4ba7f091e94ad065
Ishan <Ishan>
2017-07-26 15:23:48 +0530

    jankinsdemo

--- sel_training/testNG_framework/GridImplement.java
+++ sel_training/testNG_framework/GridImplement.java
@@ -15,40 +15,36 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class GridImplement {
-	
-	
+
 	WebDriver driver;
 	String baseurl, nodeurl;
 
 	@BeforeTest
-	public void steup() throws MalformedURLException{
-		baseurl= "http://google.com";
-		nodeurl= "http://192.168.5.60:5555/wd/hub";
+	public void steup() throws MalformedURLException {
+		baseurl = "http://google.com";
+		nodeurl = "http://192.168.5.60:5555/wd/hub";
 		DesiredCapabilities capability = DesiredCapabilities.chrome();
-		capability.setBrowserName("firefox");                           
-		capability.setPlatform(Platform.VISTA);   
-		//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\chromedriver.exe");
-	//	
-		System.setProperty("webdriver.gecko.driver", "E:\\Automationdata\\Browsers\\geckodriver.exe");
-		 driver = new FirefoxDriver();
-	//	driver = new ChromeDriver();
-		driver = new RemoteWebDriver(new URL(nodeurl), capability); 
+		capability.setBrowserName("chrome");
+		capability.setPlatform(Platform.WINDOWS);
+		 System.setProperty("webdriver.chrome.driver",
+		 System.getProperty("user.dir") + "\\chromedriver.exe");
+		
+	/*	System.setProperty("webdriver.gecko.driver", "E:\\allcodefiles\\sel_training\\geckodriver.exe");
+		driver = new FirefoxDriver();*/
+		 driver = new ChromeDriver();
+		driver = new RemoteWebDriver(new URL(nodeurl), capability);
 	}
-	
+
 	@AfterTest
-	public void afterTest()
-	{
+	public void afterTest() {
 		driver.quit();
 	}
-	
-	
+
 	@Test
-	public void SampleTest()
-	{
+	public void SampleTest() {
 		driver.get(baseurl);
+		System.out.println(driver.getTitle());
 		Assert.assertEquals("Google", driver.getTitle());
 	}
-	
-
 
 }
